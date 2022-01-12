f1e7adeef2e15aa4da427d30853c61099df80199
unknown <mbin96@konkuk.ac.kr>
2022-01-13 03:01:38 +0900

    PR#1을 참고하여 페이지 주소 변경시 자막이 안나오게 수정
    - https://github.com/mbin96/youtube_ass_subtitle/pull/1
    - PR#1에서 popup이 닫히면 동작하지 않는 현상이 있어 수정
    -- backgrund.js 추가 및 manifest에 background script로 추가
    -- PR#1에서 추가된 url 감지 스크립트를 background.js로 옮김

---
+++ src/background.js
@@ -0,0 +1,12 @@
+chrome.tabs.onUpdated.addListener(function
+    (tabId, changeInfo, tab) {
+    if (changeInfo.url) {
+
+        chrome.tabs.executeScript({
+            code: 'subtitle_instance.dispose();'
+        });
+        chrome.tabs.executeScript({
+            code: 'console.log("asdf");'
+        });
+    }
+});
