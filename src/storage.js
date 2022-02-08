396ba6e3a6598e5554391a7a2214999b2eb5330a
LaruYan KaPothoa <laruyan@users.noreply.github.com>
2022-02-08 10:50:33 +0900

    add file to base64 and reverse

--- src/storage.js
+++ src/storage.js
@@ -19,3 +19,35 @@ function getStorage(key, promise) {
     promise.resolve(got_value);
   });
 }
+
+
+// base64 문자열을 blob url로 반환합니다.
+function base64ToBlobUrl(string, mimeType) {
+  const raw = atob(string);
+  const rawLength = raw.length;
+  const array = new Uint8Array(new ArrayBuffer(rawLength));
+  for (let i = 0; i < rawLength; i++) {
+    array[i] = raw.charCodeAt(i);
+  }
+  const blob = new Blob([array], {
+    type: mimeType
+  });
+  const url = window.URL.createObjectURL(blob)
+  return url;
+}
+
+// file (input[type=file])에서 파일을 읽어 base64 문자열로 반환합니다.
+function fileToBase64(file, promise) {
+  const reader = new FileReader();
+  reader.addEventListener('load', function (loadEvent) {
+    try {
+      const uint8Array = new Uint8Array(loadEvent.target.result);
+      const base64string = ArrayBuffer.from(uint8Array).toString('base64');
+      promise.resolve(base64string);
+    } catch (e) {
+      console.error(`unable to load file : fileToBase64()`, e);
+      promise.reject(`unable to load file : fileToBase64()`,e);
+    }
+  });
+  reader.readAsArrayBuffer(file);
+}
